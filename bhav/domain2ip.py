d29cb4dc68609dc82a2d72ac7eed99bc26e0ae14
W3REW0LF <72212785+w3rew0lf@users.noreply.github.com>
2022-11-11 13:07:33 +0530

    added geolocation feature

--- bhav/domain2ip.py
+++ bhav/domain2ip.py
@@ -1,10 +1,25 @@
 import socket
+import requests
+import json
 def get_Host_name_IP(host_name): 
     try: 
         host_ip = socket.gethostbyname(host_name) 
-        print("Hostname : ",host_name) 
-        print("IP : ",host_ip) 
+        return host_ip
+        # print("Hostname : ",host_name) 
+        # print("IP : ",host_ip) 
     except: 
         print("Unable to get Hostname and IP") 
+def get_location(ip):
+    ip_address = ip
+    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+    response = requests.get(request_url)
+    result = response.content.decode()
+    result = result.split("(")[1].strip(")")
+    result  = json.loads(result)
+    return result
+
 site = input("Enter Site:")
-get_Host_name_IP(site)
+ip_addr = get_Host_name_IP(site)
+
+print(ip_addr)
+print(get_location(ip_addr))
